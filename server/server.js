02f9856362267ca824b8ecf3dca3f249816bd276
Bilal <bilal.dev121@gmail.com>
2026-01-02 09:45:08 +0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import serviceRoutes from "./routes/serviceRoutes.js";
+import appointmentRoutes from "./routes/appointmentRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/services", serviceRoutes);
+app.use("/api/appointments", appointmentRoutes);
+
+app.listen(5000, () => console.log("Server running"));
